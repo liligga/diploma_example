72751151de50519f2f5d645220ab3c0922b3f2d9
Igor <8luxfshdnlz1@mail.ru>
2022-07-21 18:34:05 +0600

    сделал шаг с добавлением одного файла

--- estate/urls.py
+++ estate/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import house_create, detail_create, house_list
+from .views import house_create, detail_create, house_list, image_create
 
 
 urlpatterns = [
     path('', house_list, name='house_list'),
     path('houses/new', house_create, name='house_create'),   
     path('<house_id>/details/new', detail_create, name='detail_create'),   
+    path('<house_id>/images/new', image_create, name='image_create'),   
 ]
--- estate/views.py
+++ estate/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from pprint import pprint
 from .models import House, HouseDetail, HouseImage
 from .forms import HouseForm, DetailForm, ImageForm
 
@@ -9,13 +10,14 @@ def house_list(request):
     context = {'houses': houses}
     return render(request, 'estate/house_list.html', context)
 
+
 @login_required
 def house_create(request):
     form = HouseForm()
-    images_form = ImageForm()
+    # images_form = ImageForm()
     if request.method == 'POST':
         form = HouseForm(request.POST)
-        images_form = ImageForm(request.FILES)
+        # images_form = ImageForm(request.FILES)
 
         if form.is_valid():
             new_house = form.save(commit=False)
@@ -24,18 +26,36 @@ def house_create(request):
             detail_form = DetailForm()
             detail_context = {'detail_form': detail_form, 'house_id': new_house.pk}
             return render(request, 'estate/partials/detail_create.html', detail_context)
-    context = {'form': form, 'images_form': images_form}
+    context = {'form': form}
     return render(request, 'estate/house_create.html', context)
+
 
 @login_required
 def detail_create(request, house_id):
-    form = DetailForm(request.POST or None)
     house = get_object_or_404(House, pk=house_id)
+    form = DetailForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
             detail_obj = form.save(commit=False)
             detail_obj.house = house
             detail_obj.save()
-            return redirect('house_list')
-    context = {'form': form}
+            image_form = ImageForm()
+            image_context = {'image_form': image_form, 'house_id': house.pk}
+            return render(request, 'estate/partials/image_create.html', image_context)
+    context = {'form': form, 'house_id': house.pk}
     return render(request, 'estate/partials/detail_create.html', context)
+
+
+@login_required
+def image_create(request, house_id):
+    house = get_object_or_404(House, pk=house_id)
+    form = ImageForm()
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            img = form.save(commit=False)
+            img.house = house
+            img.save()
+            return redirect('house_list')
+    context = {'image_form': form, 'house_id': house.pk}
+    return render(request, 'estate/partials/image_create.html', context)
